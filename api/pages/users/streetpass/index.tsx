3e953fdb104c99b675ee5b719b1232335af7b57f
Tyler Deitz <tylerdeitz@gmail.com>
2023-04-08 15:58:02 -0700

    get public key

--- api/pages/users/streetpass/index.tsx
+++ api/pages/users/streetpass/index.tsx
@@ -1,5 +1,5 @@
 import { GetServerSideProps } from "next";
-
+import * as crypto from "node:crypto";
 import type { AP } from "activitypub-core-types";
 
 export default function Page() {
@@ -109,6 +109,16 @@ export default function Page() {
 */
 
 export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
+  if (!process.env.PRIVATE_KEY) {
+    throw new Error();
+  }
+
+  const publicKeyObject = crypto.createPublicKey(process.env.PRIVATE_KEY);
+  const publicKeyString = publicKeyObject
+    .export({ format: "pem", type: "spki" })
+    .toString()
+    .replaceAll("\n", "\\n");
+
   const actor: AP.Application = {
     "@context": [
       "https://www.w3.org/ns/activitystreams",
@@ -121,8 +131,7 @@ export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
     publicKey: {
       id: "https://streetpass.social/users/streetpass#main-key",
       owner: "https://streetpass.social/users/streetpass",
-      publicKeyPem:
-        "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvXc4vkECU2/CeuSo1wtn\nFoim94Ne1jBMYxTZ9wm2YTdJq1oiZKif06I2fOqDzY/4q/S9uccrE9Bkajv1dnkO\nVm31QjWlhVpSKynVxEWjVBO5Ienue8gND0xvHIuXf87o61poqjEoepvsQFElA5ym\novljWGSA/jpj7ozygUZhCXtaS2W5AD5tnBQUpcO0lhItYPYTjnmzcc4y2NbJV8hz\n2s2G8qKv8fyimE23gY1XrPJg+cRF+g4PqFXujjlJ7MihD9oqtLGxbu7o1cifTn3x\nBfIdPythWu5b4cujNsB3m3awJjVmx+MHQ9SugkSIYXV0Ina77cTNS0M2PYiH1PFR\nTwIDAQAB\n-----END PUBLIC KEY-----\n",
+      publicKeyPem: publicKeyString,
     },
     inbox: new URL("https://streetpass.social/users/streetpass/inbox"),
     outbox: new URL("https://streetpass.social/users/streetpass/outbox"),
