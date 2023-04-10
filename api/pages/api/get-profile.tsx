40d9242343216028b51ac4134f01e2808b055cf1
Tyler Deitz <tylerdeitz@gmail.com>
2023-04-09 20:28:11 -0700

    get profile

--- api/pages/api/get-profile.tsx
+++ api/pages/api/get-profile.tsx
@@ -1,18 +1,28 @@
+import type { AP } from "activitypub-core-types";
 import type { NextApiRequest, NextApiResponse } from "next";
 import * as crypto from "node:crypto";
-import { getPrivateKey } from "../../util";
+import { convertStringsToUrls, getPrivateKey } from "../../util";
 
 export const config = {
   runtime: "nodejs",
 };
 
+type GetProfile = {
+  id: URL;
+  avatarUrl: URL | null;
+  username: string | null;
+  displayName: string | null;
+};
+
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<GetProfile>
 ) {
   try {
     const foreignTarget = new URL("https://social.chriswb.dev/@chrisw_b");
+    // const foreignTarget = new URL("https://mastodon.social/@tvler");
 
+    // https://github.com/michaelcpuckett/activitypub-core/blob/1cca3bb1355fffd56a67f6672712a2b133d8d79e/packages/activitypub-core-crypto-node/src/getHttpSignature.ts#L5
     const httpSignature = ((): {
       dateHeader: string;
       signatureHeader: string;
@@ -46,9 +56,52 @@ export default async function handler(
       throw new Error();
     }
 
-    const fetchedEntity = await fetchedEntityResp.json();
+    const fetchedEntity: unknown = await fetchedEntityResp.json();
 
-    res.json(fetchedEntity);
+    if (!fetchedEntity) {
+      throw new Error();
+    }
+
+    if (!(typeof fetchedEntity === "object")) {
+      throw new Error();
+    }
+
+    if (!("id" in fetchedEntity)) {
+      throw new Error();
+    }
+
+    const entity = convertStringsToUrls(fetchedEntity);
+
+    if (!entity.id) {
+      throw new Error();
+    }
+
+    let avatarUrl: GetProfile["avatarUrl"] = null;
+    if ("icon" in entity && !!entity.icon) {
+      if (entity.icon instanceof URL) {
+        avatarUrl = entity.icon;
+      } else if (
+        "type" in entity.icon &&
+        entity.icon.type === "Image" &&
+        entity.icon.url instanceof URL
+      ) {
+        avatarUrl = entity.icon.url;
+      }
+    }
+
+    let username: GetProfile["username"] = null;
+    if ("preferredUsername" in entity && entity.preferredUsername) {
+      username = `@${entity.preferredUsername}@${entity.id.hostname}`;
+    }
+
+    let displayName: GetProfile["displayName"] = entity.name ?? null;
+
+    res.json({
+      id: entity.id,
+      avatarUrl,
+      username,
+      displayName,
+    });
   } catch (err) {
     res.status(500).end();
   }
--- api/util.ts
+++ api/util.ts
@@ -1,3 +1,8 @@
+import { AP } from "activitypub-core-types";
+
+export const ACTIVITYSTREAMS_CONTEXT = "https://www.w3.org/ns/activitystreams";
+const PUBLIC_ACTOR = `${ACTIVITYSTREAMS_CONTEXT}#Public`;
+
 export function getPrivateKey(): string {
   if (!process.env.PRIVATE_KEY) {
     throw new Error();
@@ -6,4 +11,78 @@ export function getPrivateKey(): string {
   const privateKey = process.env.PRIVATE_KEY.split(String.raw`\n`).join("\n");
 
   return privateKey;
+}
+
+// https://github.com/michaelcpuckett/activitypub-core/blob/1cca3bb1355fffd56a67f6672712a2b133d8d79e/packages/activitypub-core-utilities/src/convertStringsToUrls.ts#L4
+export function convertStringsToUrls(originalEntity: {
+  [key: string]: unknown;
+}): AP.Entity {
+  const entity: { [key: string]: unknown } = { ...originalEntity };
+
+  for (const [key, value] of Object.entries(entity)) {
+    if (!entity) {
+      continue;
+    }
+
+    if (typeof value === "string") {
+      if (value === "as:Public") {
+        entity[key] = new URL(PUBLIC_ACTOR);
+        continue;
+      }
+      try {
+        if (value.startsWith("http")) {
+          entity[key] = new URL(value);
+        } else {
+          const date = Date.parse(value);
+
+          if (!Number.isNaN(date)) {
+            entity[key] = new Date(date);
+          }
+        }
+      } catch (error) {
+        continue;
+      }
+    } else if (value instanceof URL || value instanceof Date) {
+      continue;
+    } else if (Array.isArray(value)) {
+      entity[key] = value.map((item) => {
+        if (typeof item === "string") {
+          if (item === "as:Public") {
+            return new URL(PUBLIC_ACTOR);
+          }
+
+          try {
+            if (item.startsWith("http")) {
+              return new URL(item);
+            } else {
+              const date = Date.parse(item);
+
+              if (
+                !Number.isNaN(date) &&
+                item === new Date(date).toISOString()
+              ) {
+                return new Date(date);
+              } else {
+                return item;
+              }
+            }
+          } catch (error) {
+            return item;
+          }
+        } else if (Array.isArray(item)) {
+          return item.map((arrayItem: AP.Entity) =>
+            convertStringsToUrls(arrayItem as { [key: string]: unknown })
+          );
+        } else if (value && typeof value === "object") {
+          return convertStringsToUrls(item);
+        } else {
+          return item;
+        }
+      });
+    } else if (value && typeof value === "object") {
+      entity[key] = convertStringsToUrls(value as { [key: string]: unknown });
+    }
+  }
+
+  return entity as unknown as AP.Entity;
 }
